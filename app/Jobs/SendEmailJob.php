b78f44d18e71912eec129a11f8250dd2bf9740bb
Azim-Sabbir <mohammadsabbir044@gmail.com>
2022-06-22 16:21:16 +0600

    p-redis added

--- app/Jobs/SendEmailJob.php
+++ app/Jobs/SendEmailJob.php
@@ -6,35 +6,54 @@ use App\Mail\SendEmailMailable;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Redis\LimiterTimeoutException;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Redis;
 
 class SendEmailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    private $order;
+    private $shop;
 
     /**
      * Create a new job instance.
      *
-     * @return void
+     * @param $shop
      */
-    public function __construct($order)
+    public function __construct($shop)
     {
-        $this->order = $order;
+        $this->shop = $shop;
     }
 
     /**
      * Execute the job.
      *
      * @return void
+     * @throws LimiterTimeoutException
      */
     public function handle()
     {
-        $email = $this->order->email;
-        Mail::to($email)->send(new SendEmailMailable($this->order));
+
+
+
+
+
+        // Allow only 2 emails every 1 second
+        Redis::throttle('any_key')->allow(2)->every(1)->then(function () {
+
+            $email = $this->shop->email;
+            Mail::to($email)->send(new SendEmailMailable($this->shop));
+
+            Log::info('Emailed shop ' . $this->shop->id);
+        }, function () {
+            // Could not obtain lock; this job will be re-queued
+            return $this->release(2);
+        });
+
     }
 }
